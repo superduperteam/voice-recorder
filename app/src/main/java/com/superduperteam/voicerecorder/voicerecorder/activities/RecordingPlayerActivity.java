e467b9d53000efaf2f0b06d2df94706cf6c6c2b0
SaarAvraham <av.saar93@gmail.com>
2019-04-25 10:14:23 +0300

    polished recording_row

---
+++ app/src/main/java/com/superduperteam/voicerecorder/voicerecorder/activities/RecordingPlayerActivity.java
@@ -0,0 +1,16 @@
+package com.superduperteam.voicerecorder.voicerecorder.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.superduperteam.voicerecorder.voicerecorder.R;
+
+public class RecordingPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording_player);
+
+
+    }
+}
