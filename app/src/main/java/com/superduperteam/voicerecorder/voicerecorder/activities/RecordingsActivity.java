4256b733884fd6e16602dd9cc0ad3bd63ef9027f
SaarAvraham <av.saar93@gmail.com>
2019-04-28 15:39:37 +0300

    an attempt to make the navigator visible in the RecordingsActivity

--- app/src/main/java/com/superduperteam/voicerecorder/voicerecorder/activities/RecordingsActivity.java
+++ app/src/main/java/com/superduperteam/voicerecorder/voicerecorder/activities/RecordingsActivity.java
@@ -1,12 +1,18 @@
 package com.superduperteam.voicerecorder.voicerecorder.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.superduperteam.voicerecorder.voicerecorder.BaseActivity;
 import com.superduperteam.voicerecorder.voicerecorder.R;
 
-public class RecordingsActivity extends AppCompatActivity {
+public class RecordingsActivity extends BaseActivity {
 
     private RecyclerView recordingsRecyclerView;
 
@@ -15,7 +21,14 @@ public class RecordingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recordings);
+//        setContentView(R.layout.activity_recording_player);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        recordingsRecyclerView = findViewById(R.id.recordingsListView);
+        //inflate your activity layout here!
+        View contentView = inflater.inflate(R.layout.activity_recordings, null, false);
+        mDrawer.addView(contentView, 0);
+
+
+      //  recordingsRecyclerView = findViewById(R.id.recordingsListView);
     }
 }
