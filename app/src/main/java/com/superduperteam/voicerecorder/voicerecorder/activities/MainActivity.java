0c1a4c2f39b577a27cdb5091a96a4a6226964c8a
SaarAvraham <av.saar93@gmail.com>
2019-04-28 14:34:50 +0300

    added implementation to the voice recorder

--- app/src/main/java/com/superduperteam/voicerecorder/voicerecorder/activities/MainActivity.java
+++ app/src/main/java/com/superduperteam/voicerecorder/voicerecorder/activities/MainActivity.java
@@ -13,6 +13,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Chronometer;
@@ -21,12 +23,14 @@ import android.widget.ImageButton;
 import com.superduperteam.voicerecorder.voicerecorder.BaseActivity;
 import com.superduperteam.voicerecorder.voicerecorder.R;
 
+import java.io.IOException;
+
 public class MainActivity extends BaseActivity {
 
-    private DrawerLayout drawerLayout;
+   // private DrawerLayout drawerLayout;
     private Chronometer chronometer;
     //private boolean isStart;
-    private boolean isRecording;
+    private boolean isRecording = false;
 
     //voice recorder
     private static final String LOG_TAG = "AudioRecordTest";
@@ -35,13 +39,11 @@ public class MainActivity extends BaseActivity {
 
     private MediaRecorder recorder = null;
 
-    private MediaPlayer player = null;
-
     // Requesting permission to RECORD_AUDIO
     private boolean permissionToRecordAccepted = false;
     private String[] permissions = {Manifest.permission.RECORD_AUDIO};
 
-    boolean mStartRecording = true;
+   // boolean mStartRecording = true;
 
 
     @Override
@@ -65,23 +67,31 @@ public class MainActivity extends BaseActivity {
 //        drawerLayout = findViewById(R.id.drawer_layout);
 
         // Record to the external cache directory for visibility
-        fileName = getExternalCacheDir().getAbsolutePath();
+//        fileName = getExternalCacheDir().getAbsolutePath();
+//        fileName += "/audiorecordtest.3gp";
+        fileName = getExternalFilesDir(null).getAbsolutePath();
         fileName += "/audiorecordtest.3gp";
+        Log.i(LOG_TAG,  "filePath: " + fileName);
 
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
+        chronometerInit();
+    }
 
 
-
+    private void chronometerInit() {
         chronometer = findViewById(R.id.chronometer);
 
+        chronometer.setBase(SystemClock.elapsedRealtime()); // new
         chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometerChanged) {
                 chronometer = chronometerChanged;
             }
         });
+
     }
+
 
 //    @Override
 //    public boolean onOptionsItemSelected(MenuItem item) {
@@ -95,33 +105,133 @@ public class MainActivity extends BaseActivity {
 
 
     private long lastPause;
-    private boolean isFirstStart = true;
+
+    //private boolean isFirstStart = true;
+    // Saar: changed some things (added stop option so a recording could be stopped and saved).
+    // using isRunning instead of isFirstStart. isRunning as whether a recorder is even running or not.
+    private boolean isRunning = false;
 
     public void onRecordClick(View view) {
         ImageButton button = findViewById(R.id.record_button);
         button.setBackgroundResource(R.drawable.ic_pressed_record_button);
 
         if (!isRecording) {
+            //startStopWatch(isFirstStart);
+
             startStopWatch();
+            startRecording(isRunning);
+            isRunning = true;
         } else {
-            stopStopWatch();
+            //  pauseStopWatch();
+
+            pauseStopWatch();
+            pauseRecording();
         }
 
         isRecording = !isRecording;
     }
 
-    private void stopStopWatch() {
-        lastPause = SystemClock.elapsedRealtime();
-        chronometer.stop();
+    public void onStopClick(View view) {
+        ImageButton button = findViewById(R.id.stop_button);
+
+        resetStopWatch();
+        stopRecording();
+        isRunning = false;
+        isRecording = false;
+        //stopStopWatch();
+    //    stopRecording();
     }
 
+//    private void stopStopWatch() {
+//        chronometer.setBase(SystemClock.elapsedRealtime());
+//        chronometer.stop();
+//        lastPause = 0;
+//    }
+//    private void pauseStopWatch() {
+//        lastPause = SystemClock.elapsedRealtime();
+//        chronometer.stop();
+//    }
+//    private void startStopWatch(boolean isFirstStart) {
+//        if (isFirstStart) {
+//            chronometer.setBase(SystemClock.elapsedRealtime());
+//        } else {
+//            chronometer.setBase(chronometer.getBase() + SystemClock.elapsedRealtime() - lastPause);
+//        }
+//        chronometer.start();
+//    }
+
+private long pauseOffset = 0;
+
     private void startStopWatch() {
-        if (isFirstStart) {
-            isFirstStart = false;
-            chronometer.setBase(SystemClock.elapsedRealtime());
-        } else {
-            chronometer.setBase(chronometer.getBase() + SystemClock.elapsedRealtime() - lastPause);
+//        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+       //     running = true;
+       // }
+    }
+
+    private void pauseStopWatch() {
+      //  if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+      //      running = false;
+       // }
+    }
+
+    private void resetStopWatch() {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.stop();
+        pauseOffset = 0;
+
+        ImageButton button = findViewById(R.id.record_button);
+        button.setBackgroundResource(R.drawable.ic_record_button);
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+
+        startPlaying();
+    }
+
+    private void pauseRecording() {
+        recorder.pause();
+    }
+
+    private void startPlaying() {
+        MediaPlayer player = new MediaPlayer();
+        try {
+            player.setDataSource(fileName);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
         }
-        chronometer.start();
+    }
+
+    private void recorderInit() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(fileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        recorder.start();
+    }
+
+    private void startRecording(boolean isRunning) {
+        if(!isRunning){
+            recorderInit();
+        }
+        else{
+            recorder.resume();
+        }
     }
 }
